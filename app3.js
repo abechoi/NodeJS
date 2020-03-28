b35f0d4a9645307ea74d1d0400d8ac1dc09a31e2
abechoi <abraham.choi@icloud.com>
2020-03-27 23:41:27 -0700

    HTTP, Pipes, and Streams

--- app3.js
+++ app3.js
@@ -1,15 +1,28 @@
 // ! HTTP - Requests and Responses
-var http = require('http');
+// ! Pipes and Streams - Readable and Writable
 
-// TODO: Create a server with a function that contains 2 arguments.
+// TODO: Create a server that pipes readable data into a response.
 // A request consists of request + request header.
 // A response consists of response data + response header.
+var http = require('http');
+var fs = require('fs');
+/*
 var server = http.createServer(function(req, res){
     console.log('Request was made: '+req.url); // displays message in console when a request is made.
     res.writeHead(200, {'Content-Type': 'text/plain'}); // Status: 200, Content: plain text
-    res.end('Hello World!'); // Shows on the browser
-});
 
-// TODO: Have the server listen for a request.
+    var myReadStream = fs.createReadStream(__dirname + '/readstream.txt', 'utf8');
+    myReadStream.pipe(res);
+});
 server.listen(3000, '127.0.0.1'); // port:3000, localhost
-console.log('Listening to port:3000...');
+console.log('Listening to port: 3000...');
+*/
+// TODO: Read a file into new file using streams.
+// Read the data chunks and write the chunks into the new file.
+var myReadStream = fs.createReadStream(__dirname + '/readstream.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/writestream.txt');
+myReadStream.on('data', (chunk) => (
+    console.log('New chunk received'),
+    myWriteStream.write(chunk)
+));
+
--- practice/app3.js
+++ practice/app3.js
@@ -1,15 +1,29 @@
 // ! HTTP - Requests and Responses
-var http = require('http');
+// ! Pipes and Streams - Readable and Writable
 
-// TODO: Create a server with a function that contains 2 arguments.
+// TODO: Create a server that pipes readable data into a response.
 // A request consists of request + request header.
 // A response consists of response data + response header.
-var server = http.createServer(function(req, res){
-    console.log('Request was made: '+req.url); // displays message in console when a request is made.
-    res.writeHead(200, {'Content-Type': 'text/plain'}); // Status: 200, Content: plain text
-    res.end('Hello World!'); // Shows on the browser
-});
+var http = require('http');
+var fs = require('fs');
 
-// TODO: Have the server listen for a request.
-server.listen(3000, '127.0.0.1'); // port:3000, localhost
-console.log('Listening to port:3000...');
+var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+var server = http.createServer((req, res) => (
+    console.log('Requesting URL: ' + req.url),
+    res.writeHead(200, {'Content-Type': 'text/plain'}),
+    readStream.pipe(res)
+));
+server.listen(3000, '127.0.0.1');
+console.log('Listnening to port: 3000...');
+
+
+// TODO: Read a file into new file using streams.
+// Read the data chunks and write the chunks into the new file.
+/*
+var readStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+var writeStream = fs.createWriteStream(__dirname + '/writeme.txt');
+readStream.on('data', (chunk) => (
+    console.log('New chunk received.'),
+    writeStream.write(chunk)
+));
+*/
